70e1dc937d97ac10f856a67882ee960df4754616
lexria <alexandriamlewis@me.com>
2016-02-16 10:15:31 -0500

    mostly did the array methods

--- jsFunc01.js
+++ jsFunc01.js
@@ -113,33 +113,127 @@ function longest(){
  * 2. write a console.assert to test them
  */
 
- var myNames = ["Sophia", "Ezelene", "Otto", "Etta", "Brianne", "Star"];
+var myNames = ["Sophia", "Ezelene", "Otto", "Etta", "Brianne", "Star"];
+var myNumbers = [2, 4, 6, 8, 1, 3];
+var myString = "This is a string to test on.";
 
-console.assert(myNames.sort());
+var myNamesSort = (function(){
+  console.log(myNames);
+  console.log("SORTED " + myNames.sort());
+  console.log(myNumbers);
+  console.log("SORTED " + myNumbers.sort());
+});
+
+// console.assert(myNames.sort());
+// console.assert(myNumbers.sort());
+
 // .sort()
 
-console.assert(myNames.concat());
+var myNamesConcat = (function(){
+  console.log(myNames);
+  console.log(myNumbers);
+  var nameNumber = myNames.concat(myNumbers);
+  console.log(nameNumber);
+});
+
+// var nameNumber = myNames.concat(myNumbers);
+// console.assert(nameNumber);
+
 // .concat()
 
-console.assert(myNames.indexOf());
+var myNamesIndex = (function(){
+  console.log(myNames);
+  console.log(myNames.indexOf("Otto"));
+});
+
+// console.assert(myNames.indexOf("Otto"));
+
 // .indexOf()
+
+var myStringSplit = (function(){
+  console.log(myString);
+  console.log(myString.split(" ")); //splits string value by " " into an array
+});
+
+// console.assert(myString.split(" "));
 
 // .split()
 
+var myNamesJoin = (function(){
+  console.log(myNames);
+  console.log(myNames.join(", ")); //creates a string from array with ", " inbetween each index
+});
+
+// console.assert(myNames.join(", "));
+
 // .join()
+
+var myNumbersPop = (function(){
+  console.log(myNumbers);
+  console.log(myNumbers.pop()); //removes from the end of array ???parameters???
+  console.log(myNumbers);
+});
+
+// console.assert(myNumbers.pop());
 
 // .pop()
 
+var myNumbersPush = (function(){
+  console.log(myNumbers);
+  console.log(myNumbers.push(5, 7, 9)); //adds to the end of array
+  console.log(myNumbers);
+});
+
 // .push()
+
+var myNamesSlice = (function(){
+  console.log(myNames);
+  console.log(myNames.slice(2, 5));
+  // (index spot, how far from that spot to go) takes out indexes
+  // will return ["Otto", "Etta", "Brianne"] ????? why 5?????
+  console.log(myNames);
+});
 
 // .slice()
 
+var myNumbersSplice = (function(){
+  console.log(myNumbers);
+  console.log(myNumbers.splice(2, 0, "potato")); //insert new index
+  console.log(myNumbers);
+  console.log(myNumbers.splice(2, 4)); //deleting indexes and returns those indexes and values
+  console.log(myNumbers);
+});
+
 // .splice()
+
+var myNamesShift = (function(){
+  console.log(myNames);
+  console.log(myNames.shift()); //removes from front and returns that element ???parameters???
+  console.log(myNames);
+});
 
 // .shift()
 
+var myNumbersUnshift = (function(){
+  console.log(myNumbers);
+  console.log(myNumbers.unshift(10, 11, 12)); //adds to the front of array returns new length ???parameters???
+  console.log(myNumbers);
+});
+
 // .unshift()
 
+var myNumbersFilter = (function(){
+  console.log(myNumbers);
+  console.log(myNumbers.filter());
+  console.log(myNumbers);
+});
+
 // .filter()
+
+var myNamesMap = (function(){
+  console.log(myNames);
+  console.log(myNames.map());
+  console.log(myNames);
+});
 
 // .map()
